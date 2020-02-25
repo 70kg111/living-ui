7e5c8dbcff0e3b9db4180918a467f857fcbfa00e
70kg111 <76104718@qq.com>
2020-02-25 23:47:07 +0800

    初步使用vue press

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # living-ui 一个简单的UI库
 
-[![Build Status](https://www.travis-ci.org/70kg111/living-ui.svg?branch=master)](https://www.travis-ci.org/70kg111/living-ui)
-
 作者：living
 
 ## 介绍
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: 'Living UI',
+  description: '一个易用的UI框架',
+  themeConfig: {
+    sidebar: [
+      '/',
+      '/install/',
+      '/get-started/',
+    ],
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '源码', link: '/guide/' },
+      { text: 'GitHub', link: 'https://github.com/70kg111' },
+    ]
+  }
+};
